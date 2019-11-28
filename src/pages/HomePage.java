cbc51821d4a39bf202f251517d8a95ebec08863b
Jeetsharma1223 <Jeetsharma1223@gmail.com>
2019-11-27 21:05:26 -0500

    Welcome file added Home pagge deleted

--- src/pages/HomePage.java
+++
@@ -1,7 +0,0 @@
-package pages;
-
-public class HomePage {
-
-	///////////hiiiiiiiiiii
-	//System.out.println("Welcome to home pahe");
-}
---
+++ src/pages/WelcomePage.java
@@ -0,0 +1,7 @@
+package pages;
+
+public class WelcomePage {
+
+	
+	/// weclcome page added
+}
